fce63dfd71c076eacbc01ebf0b2867f1073ece32
DaveMag <davidemao86@gmail.com>
2025-04-21 17:05:54 +0200

    create navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Profile from './pages/profile'
+import SignIn from './pages/sign-in'
+import SignOut from './pages/sign-out'
 
 const App = () => {
   return (
-    <h1 className='text-3xl text-center text-amber-950'>
-      Hello There
-    </h1>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='/about' element={<About />}/>
+    <Route path='/profile' element={<Profile />}/>
+    <Route path='/sign-in' element={<SignIn />}/>
+    <Route path='/sign-out' element={<SignOut />}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
