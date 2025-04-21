b3bcecc6cf159b62fabdca1d3598e04df4687ed9
DaveMag <davidemao86@gmail.com>
2025-04-21 17:37:39 +0200

    create header and nav

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import About from './pages/about'
 import Profile from './pages/profile'
 import SignIn from './pages/sign-in'
 import SignOut from './pages/sign-out'
+import Header from './components/header'
 
 const App = () => {
   return (
    <BrowserRouter>
+   <Header />
    <Routes>
     <Route path='/' element={<Home />}/>
     <Route path='/about' element={<About />}/>
